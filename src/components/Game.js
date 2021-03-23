45eea4b0ac9699418a49713302668aaefffab227
Julian <julianrosenthal@yahoo.com>
2021-03-22 21:17:03 -0700

    Done.

--- src/components/Game.js
+++ src/components/Game.js
@@ -17,6 +17,9 @@ class Game extends React.Component {
   }
 
   // TODO: create a componentDidMount() which will set the current time
+  componentDidMount() {
+    this.setCurrentTime();
+  }
   
   setCurrentTime = () => {
     this.setState({ time: new Date(Date.now())});
@@ -40,6 +43,7 @@ class Game extends React.Component {
   }
 
   render() {
+    console.log(this.state)
     const { pancakes, burnt, cooked, raw, time } = this.state;
     const pans = pancakes.map((pancake, index) => <Pancake key={index} id={pancake} takeItOff={this.takeItOff} />);
 
